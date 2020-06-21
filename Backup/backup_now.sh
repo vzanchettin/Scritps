a68e40b95d644022ae2771859ea23160ccfacd69
vzanchettin <32183401+vzanchettin@users.noreply.github.com>
2020-06-20 22:44:09 -0300

    Add files via upload

--- Backup/backup_now.sh
+++ Backup/backup_now.sh
@@ -10,7 +10,8 @@ SET_LOGFILE="/var/log/backup_now.log";
 
 # Change if you do a tar backup files
 SET_TAR_BACKUP_STORAGE="${SET_BACKUP_STORAGE}/tar-backups";
-SET_TAR_FOLDER_TO_COPY="/var /etc";
+SET_TAR_FOLDER_TO_COPY="/var/spool /etc /usr/bin"; # Where you can input some folders to backup, like /var/www /home.
+SET_DAY_TO_BACKUP_FULL="1"; # Where 1 (mon), 2 (tue), 3 (wed), 4 (thu), 5 (fri) and 6 (sat).
 
 echo "" >> ${SET_LOGFILE};
 echo "##########################################" >> ${SET_LOGFILE};
@@ -47,9 +48,6 @@ function create_base_directories {
 
 }
 
-
-
-
 function backup_with_tar {
 
 	GET_DATE=`date +%d-%m-%Y`;
@@ -58,40 +56,41 @@ function backup_with_tar {
 
 	for FOLDER in ${SET_TAR_FOLDER_TO_COPY}; do
 
-		if [ -e ${SET_TAR_BACKUP_STORAGE}${FOLDER} ]; then
+		GET_FOLDER_NAME=`echo ${FOLDER} | sed 's/\///g'`;
 
-			echo "Folder ${SET_TAR_BACKUP_STORAGE}${FOLDER} already exist." >> ${SET_LOGFILE};
+		if [ -e ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME} ]; then
+
+			echo "Folder ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME} already exist." >> ${SET_LOGFILE};
 
 		else
 
-			mkdir ${SET_TAR_BACKUP_STORAGE}${FOLDER};
+			echo "${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME} does not exist, creating..." >> ${SET_LOGFILE};
+			mkdir ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME};
 
 		fi
-
-
 
 		GET_DATE=`date +%d-%m-%Y`;
 		GET_TIME=`date +%H-%M-%S`;
 		GET_DAY=`date +%w`;
 
-		if [ ${GET_DAY} = 5 ]; then
+		if [ ${GET_DAY} = ${SET_DAY_TO_BACKUP_FULL} ]; then
 
-			echo "Day is 1, moving last full backup to a temp file" >> ${SET_LOGFILE};
+			echo "Day is ${SET_DAY_TO_BACKUP_FULL}, moving last full backup to a temp file" >> ${SET_LOGFILE};
 
-			mv  ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}_${GET_DAY}.tar.gz  ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}_${GET_DAY}.tar.gz.old;
-			rm -rf ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}.snap
+			mv  ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}_${GET_DAY}.tar.gz  ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}_${GET_DAY}.tar.gz.old;
+			rm -rf ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}.snap
 
 		fi
 
 		echo "Starting backup of ${FOLDER} folder." >> ${SET_LOGFILE};
-		tar -cvzf ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}_${GET_DAY}.tar.gz ${FOLDER} --listed-incremental=${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}.snap --verbose
+		tar -cvzf ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}_${GET_DAY}.tar.gz ${FOLDER} --listed-incremental=${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}.snap --verbose
 		echo "Backup folder ${FOLDER} finished." >> ${SET_LOGFILE};
 
 
-		if [ -e ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}_${GET_DAY}.tar.gz ]; then
+		if [ -e ${SET_TAR_BACKUP_STORAGE}/${GET_FOLDER_NAME}/${GET_FOLDER_NAME}_${GET_DAY}.tar.gz.old ]; then
 
 			echo "Removing an old backup." >> ${SET_LOGFILE};
-			rm -rf ${SET_TAR_BACKUP_STORAGE}${FOLDER}${FOLDER}_${GET_DAY}.tar.gz.old; 
+			rm -rf ${SET_TAR_BACKUP_STORAGE}${FOLDER}/${GET_FOLDER_NAME}_${GET_DAY}.tar.gz.old; 
 
 		fi
 
